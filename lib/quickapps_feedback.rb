fc6a2eff6ce053a88ac135a42bc0662a8652b214
Grant Speelman <grant.speelman@ubxd.com>
2014-06-17 20:36:55 +0200

    add sinatra

--- lib/quickapps_feedback.rb
+++ lib/quickapps_feedback.rb
@@ -1,5 +1,11 @@
 require 'quickapps_feedback/version'
+require 'sinatra/base'
 
 module QuickappsFeedback
   # Your code goes here...
+  class App < Sinatra::Application
+    get '/feedback' do
+      'feedback here'
+    end
+  end
 end
--- test/sample_test.rb
+++ test/sample_test.rb
@@ -1,7 +1,16 @@
 require 'minitest/autorun'
+require 'rack/test'
+require '../lib/quickapps_feedback'
 
 describe 'test' do
-  it 'runs' do
-    true.wont_equal false
+  include Rack::Test::Methods
+
+  def app
+    QuickappsFeedback::App
+  end
+
+  def test_hello_world
+    get '/feedback'
+    last_response.must_be(:ok?)
   end
 end
